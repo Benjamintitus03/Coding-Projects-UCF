300d04515d74c933beb868afa7f740aefbc9b568
flas0e <160093052+flas0e@users.noreply.github.com>
2024-04-11 22:57:50 -0400

    Update lab9-1.c

--- lab9-1.c
+++ lab9-1.c
@@ -1,94 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_SIZE 10
 
 // RecordType
-struct RecordType
-{
-	int		id;
-	char	name;
-	int		order; 
+struct RecordType {
+    int id;
+    char name;
+    int order;
 };
 
-// Fill out this structure
-struct HashType
-{
+// Node structure for linked list
+struct Node {
+    struct RecordType data;
+    struct Node *next;
+};
 
+// Hash table structure
+struct HashType {
+    struct Node *head;
 };
 
 // Compute the hash function
-int hash(int x)
-{
-
+int hash(int x) {
+    return x % HASH_SIZE;
 }
 
-// parses input file to an integer array
-int parseData(char* inputFileName, struct RecordType** ppData)
-{
-	FILE* inFile = fopen(inputFileName, "r");
-	int dataSz = 0;
-	int i, n;
-	char c;
-	struct RecordType *pRecord;
-	*ppData = NULL;
-
-	if (inFile)
-	{
-		fscanf(inFile, "%d\n", &dataSz);
-		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
-		// Implement parse data block
-		if (*ppData == NULL)
-		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
-		}
-		for (i = 0; i < dataSz; ++i)
-		{
-			pRecord = *ppData + i;
-			fscanf(inFile, "%d ", &n);
-			pRecord->id = n;
-			fscanf(inFile, "%c ", &c);
-			pRecord->name = c;
-			fscanf(inFile, "%d ", &n);
-			pRecord->order = n;
-		}
-
-		fclose(inFile);
-	}
-
-	return dataSz;
+// Function to create a new node
+struct Node *createNode(struct RecordType data) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-// prints the records
-void printRecords(struct RecordType pData[], int dataSz)
-{
-	int i;
-	printf("\nRecords:\n");
-	for (i = 0; i < dataSz; ++i)
-	{
-		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
-	}
-	printf("\n\n");
+// Function to insert record into hash table
+// Function to insert record into hash table
+void insert(struct HashType *hashArray, int hashSize, struct RecordType record) {
+    int index = hash(record.id);
+    struct Node *newNode = createNode(record);
+    newNode->next = hashArray[index].head;
+    hashArray[index].head = newNode;
 }
 
-// display records in the hash structure
-// skip the indices which are free
-// the output will be in the format:
-// index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
-{
-	int i;
 
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+// Function to display records in the hash table
+void displayRecordsInHash(struct HashType *hashArray, int hashSize) {
+    printf("Records in Hash Table:\n");
+    for (int i = 0; i < hashSize; i++) {
+        struct Node *temp = hashArray[i].head;
+        printf("Index %d -> ", i);
+        while (temp != NULL) {
+            printf("(%d, %c, %d) -> ", temp->data.id, temp->data.name, temp->data.order);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
 }
 
-int main(void)
-{
-	struct RecordType *pRecords;
-	int recordSz = 0;
+// Function to parse input file and fill records
+int parseData(char *inputFileName, struct RecordType **ppData) {
+    FILE *inFile = fopen(inputFileName, "r");
+    int dataSz = 0;
+    struct RecordType *pRecord;
+    *ppData = NULL;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+    if (inFile) {
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (struct RecordType *)malloc(sizeof(struct RecordType) * dataSz);
+        if (*ppData == NULL) {
+            fprintf(stderr, "Cannot allocate memory\n");
+            exit(1);
+        }
+        for (int i = 0; i < dataSz; ++i) {
+            pRecord = *ppData + i;
+            fscanf(inFile, "%d %c %d\n", &pRecord->id, &pRecord->name, &pRecord->order);
+        }
+        fclose(inFile);
+    }
+
+    return dataSz;
+}
+
+// Function to print records
+void printRecords(struct RecordType *pData, int dataSz) {
+    printf("\nRecords:\n");
+    for (int i = 0; i < dataSz; ++i) {
+        printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    char inputFileName[100];
+    printf("Enter the name of the input file: ");
+    scanf("%s", inputFileName);
+
+    struct RecordType *pRecords;
+    int recordSz = parseData(inputFileName, &pRecords);
+    printRecords(pRecords, recordSz);
+
+    // Create hash table
+    struct HashType hashArray[HASH_SIZE] = {0};
+
+    // Insert records into hash table
+    for (int i = 0; i < recordSz; i++) {
+        insert(hashArray, HASH_SIZE, pRecords[i]);
+    }
+
+    // Display records in the hash table
+    displayRecordsInHash(hashArray, HASH_SIZE);
+
+    // Free allocated memory
+    free(pRecords);
+
+    return 0;
 }
